153e1ca3e69f1eae54f9046ce30c7b4d5e28151e
Asmita Raj <letmebe.asmita@gmail.com>
2020-12-31 15:05:54 +0530

    Add profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profile"""
+    serializer_class = serializers.UserProfileSerializers
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
