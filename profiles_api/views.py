c7d6018eb0ccfcb0b253802072b2c51cb41c8f24
Asmita Raj <letmebe.asmita@gmail.com>
2020-12-31 16:03:35 +0530

    Add search filer to profiles API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
@@ -12,3 +13,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email')
